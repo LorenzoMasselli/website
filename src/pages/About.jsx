895a377ac8042432bdc592ffc21fd78773e8d6d8
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2024-03-03 13:12:38 +0000

    about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,80 @@
 // import React from 'react'
 
+import "../styling/About.css"
+
 function About() {
   return (
-    <div>About</div>
+    <>
+      <div className="about__container">
+            <div className='about__me'>
+                <h4 className='about__header'>Studied Web Development at Le Wagon Bootcamp as well as Computer Science Modules during my Bachelors at Bocconi in Milan</h4>
+                <p className='about__header__subtext'>1+ years of experience in coding, designing & building web applications. Prior to this, I worked in FinTech, analyzing complex data and making strategic decisions. I also played professional rugby for 7 years.</p>
+            </div>
+            <div className='horizontal-line-container'><div className='horizontal-line-about'></div></div>
+            <div className='about__about'>
+              <h4 className='home__header'>Work Experience</h4>
+              <div className="work__block__container">
+                <div className="work__experience__container">
+                  <p className="work__date">2024</p>
+                  <p className="work__company">Beach House</p>
+                  <span className="work__role">Freelance Web Design</span>
+                </div>
+                <div className="work__experience__container">
+                  <p className="work__date"> 2022 –– 2023</p>
+                  <p className="work__company">Pollinate</p>
+                  <span className="work__role">Business Analyst</span>
+                </div>
+                <div className="work__experience__container">
+                  <p className="work__date">2016 –– 2021</p>
+                  <p className="work__company">Zebre | Lyons</p>
+                  <span className="work__role">Professional Rugby Player</span>
+                </div>
+              </div>
+              <p className='about__overview'>Throughout my work experience, I have developed critical thinking and analytical abilities, which enable me to approach complex problems strategically and deliver high quality solutions. My experience & my career in professional sports has enabled me to work really well in a high performance team environment, where I collaborate and communicate well. These skills empower me to thrive in the dynamic tech industry.</p>
+            </div>
+            <div className='horizontal-line-container'><div className='horizontal-line-about'></div></div>
+            <div className='about__skills'>
+              <h4 className='home__header'>Skills</h4>
+              <div className="skills__block__container">
+                <div className="skill__block">
+                  <span className="skill__head">Languages</span>
+                  <div className="skills__tech">
+                    <span className="skill__bubble">Javascript</span>
+                    <span className="skill__bubble">Typescript</span>
+                    <span className="skill__bubble">Ruby</span>
+                    <span className="skill__bubble">SQL</span>
+                    <span className="skill__bubble">CSS</span>
+                    <span className="skill__bubble">HTML</span>
+                  </div>
+                </div>
+              </div>
+              <div className="skills__block__container">
+                <div className="skill__block">
+                  <span className="skill__head">Frameworks & Libraries</span>
+                  <div className="skills__tech">
+                    <span className="skill__bubble">React</span>
+                    <span className="skill__bubble">Jest</span>
+                    <span className="skill__bubble">Bootstrap</span>
+                    <span className="skill__bubble">Ruby on Rails</span>
+                  </div>
+                </div>
+              </div>
+              <div className="skills__block__container">
+                <div className="skill__block">
+                  <span className="skill__head">Other</span>
+                  <div className="skills__tech">
+                    <span className="skill__bubble">Git</span>
+                    <span className="skill__bubble">Node.js</span>
+                    <span className="skill__bubble">Github</span>
+                    <span className="skill__bubble">Figma</span>
+                    <span className="skill__bubble">Heroku</span>
+                    <span className="skill__bubble">PostgreSQL</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,68 +1,157 @@
 // import React from 'react'
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import savore from "../assets/savore_home.png"
 import savoreDashboard from "../assets/dashboard_savore.png"
+import savoreCalendar from "../assets/savore_calendar.png"
+import wikinimous from "../assets/wikinimous.png"
+import barhopCrawl from "../assets/barhop_crawl.png"
 import barhop from "../assets/barhop.png"
 import render from "../assets/render.png"
+import renderForm from "../assets/render_form.png"
 import "../styling/Work.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 
 function Work() {
+const [collapsedItems, setCollapsedItems] = useState("");
+
+  const workInfo = {
+    1: {
+      image: [render, renderForm],
+      heading: "Render",
+      subHeading: "Design & Development",
+      industry: "Art",
+      description: "Contributed to the development of a peer-to-peer web application that allows users to rent and share artworks with others, with a focus on making art accessible to everyone.",
+      github: "Github",
+      website: "",
+      tech: ["Ruby on Rails", "Javascript", "PostgreSQL", "Figma"],
+      githubLink: "https://github.com/AlekP141/Render",
+      websiteLink: "",
+    },
+    2: {
+      image: [savoreDashboard, savore, savoreCalendar ],
+      heading: "Savore",
+      subHeading: "Design & Development",
+      industry: "Restaurant",
+      description: "Developed a restaurant reservation platform featuring dynamic routing, API-driven data retrieval, user input handling, and secure user authentication. Rigorous Jest unit tests were implemented for code reliability. The platform seamlessly integrates API requests for efficient user and booking data management, providing a smooth experience for both customers and staff.",
+      github: "Github",
+      website: "Website",
+      tech: ["React", "Node.js", "Ruby on Rails", "PostgreSQL", "Jest"],
+      githubLink: "https://github.com/LorenzoMasselli/react-rails-restaurant-frontend",
+      websiteLink: "https://lorenzomasselli.github.io/react-rails-restaurant-frontend/",
+  
+    },
+    3: {
+      image: [barhop, barhopCrawl],
+      heading: "Barhop",
+      subHeading: "Design & Development",
+      industry: "Entertainment",
+      description: "Contributed to the development of BarHop, a web application that generates curated bar crawl routes based on user preferences like venue type, price, and the number of bars. Integrated external Restful APIs for real-time access to comprehensive bar venue data, enhancing the user experience.",
+      github: "Github",
+      website: "",
+      tech: ["Ruby on Rails", "Javascript", "PostgreSQL", "Figma"],
+      githubLink: "https://github.com/dyloff/barhop",
+      websiteLink: "",
+    },
+    4: {
+      image: [wikinimous],
+      heading: "Wikinimous",
+      subHeading: "Design & Development",
+      industry: "Encyclopedia",
+      description: "I built a wiki website where volunteers can create and update articles for people to read. An online encyclopedia of information.",
+      github: "Github",
+      website: "",
+      tech: ["Ruby on Rails", "Javascript", "Bootstrap"],
+      githubLink: "https://github.com/LorenzoMasselli/rails-wikinimous",
+      websiteLink: "",
+    },
+
+  }
+
+  const handleCollapseToggle = (id) => {
+    const item = document.getElementById(id)
+    const allCollapsed = document.querySelectorAll(".collapsed")
+    allCollapsed.forEach((collapsedItem) => {
+      if (collapsedItem !== item) {
+        collapsedItem.classList.remove('collapsed');
+      }
+    });
+    if (item.classList.contains("collapsed")) {
+      setCollapsedItems("")
+    } else {
+  
+      setCollapsedItems(id)
+    }
+    item.classList.toggle('collapsed');
+  }
+
   return (
     <>
-      <div className='my__work'>
-            <h4 className='home__header'>My Work</h4>
-            <div className="my_work_list">
-                <ul className='my__work__container'>
-                    <li>
-                        <div className='my__work__item'>
-                          <img className="work__image__hover" src={savore} alt="render website image"/>
-                          <div className='my__header'>Savore</div>
-                          <div className="my__design">Design & Development</div>
-                          <div className='my__tech'>
-                              <div>Restaurant</div>
-                          </div>
-                        </div>
-                        <div className="work__more">
-                          <img className="work__image" src={savore} alt="savore website image"/>
-                          <img className="work__image__2" src={savoreDashboard} alt="saviore website dashboard"/>
-                          <div className='column_grid_3 tech__work'>
-                              <p className='tech__bubble'>React</p>
-                              <p className='tech__bubble'>PostgreSQL</p>
-                              <p className='tech__bubble'>Jest</p>
-                              <p className='tech__bubble'>Figma</p>
-                              <p className='tech__bubble'>Javascript</p>
-                              <p className='tech__bubble'>Node.js</p>
-                              <p className='tech__bubble'>Git</p>
-                              <p className='tech__bubble'>Ruby on Rails</p>
-                          </div>
-                          <div className="work__description">
-                            <p className="work__description__text">Developed a restaurant reservation platform featuring dynamic routing, API-driven data retrieval, user input handling, and secure user authentication. Rigorous Jest unit tests were implemented for code reliability. The platform seamlessly integrates API requests for efficient user and booking data management, providing a smooth experience for both customers and staff.</p>
-                            <p className="work__link">Github</p>
-                            <p className="work__link">Live Site</p>
-                          </div>
-                        </div>
+      <div className='work'>
+            <h4 className='work__subheader'>From ideation to deployment, I turn web app goals into reality through innovative design and development</h4>
+            <h4 className='work__header'>My Work</h4>
+            <div className="work__container">
+                <ul className='work__list'>
+                  {Object.values(workInfo).map((work, i) => (
+                    <li key={work.heading} className='work__item'>
+                      <div className="work__collapsable" onClick={() => handleCollapseToggle(i)}>
+                        <h5 className="work__list__item__head">{work.heading}</h5>
+                        <h5 className="work__list__item__subhead">{work.subHeading}</h5>
+                        <h5 className="work__list__item__industry">{work.industry}</h5>
+                        <span className="chevron__down"><FontAwesomeIcon style={{
+                        transform: collapsedItems === i
+                          ? "rotate(180deg)"
+                          : "rotate(0deg)",
+                      }} icon={faChevronDown} /></span>
+                      </div>
+                      <div className="work__collapsed" id={i}>
+                        <img className="work__image" src={work.image[0]} alt={work.heading} />
+                        {work.image[1] && (
+                          <img className="work__image__two" src={work.image[1]} alt={work.heading} />
+                        )}
+                        {work.image[2] && (
+                          <img className="work__image__three" src={work.image[2]} alt={work.heading} />
+                        )}
+                        <p className="work__github__website">
+                          <Link className="unlink" target="_blank" to={work.githubLink} >
+                            <span className="link work__gh__w">
+                              {work.github} 
+                              <span>
+                                <FontAwesomeIcon icon={faArrowRight} />
+                              </span>
+                            </span>
+                          </Link>
+                          {work.websiteLink && (
+                            <Link className="unlink" target="_blank" to={work.websiteLink}>
+                              <span className="link work__gh__w">
+                                {work.website ? (
+                                  <>
+                                    {work.website}
+                                    <span><FontAwesomeIcon icon={faArrowRight} /></span>
+                                  </>
+                                ) : null}
+                              </span>
+                            </Link>
+                          )}
+
+                        </p>
+                        <p className="work__description">{work.description}</p>
+                        <span className="work__tech">
+                          {work.tech.map((tech, index) => (
+                            <p key={index}>{tech}</p>
+                          ))}
+                        </span>
+                      </div>
                     </li>
-                    <li className='my__work__item'>
-                        <img className="work__image__hover" src={barhop} alt="render website image"/>
-                        <div className='my__header'>Barhop</div>
-                        <div>Design & Development</div>
-                        <div className='tech'>
-                            <div>Entertainment</div>
-                        </div>
-                    </li>
-                    <li className='my__work__item'>
-                        <img className="work__image__hover" src={render} alt="render website image"/>
-                        <div className='my__header'>Render</div>
-                        <div>Design & Development</div>
-                        <div className='tech'>
-                            <div>Art</div>
-                        </div>
-                    </li>
+                  ))}
                 </ul>
             </div>
         </div>
     </>
   )
 }
+
 
 export default Work
