616f5015b7748711f9561de05241bc693049dfd7
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2024-03-04 12:07:11 +0000

    added vercel performance metrics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from "./components/Navbar"
 import AppRoutes from "./routes/AppRoutes"
 import Footer from "./components/Footer"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
 
